79f92ea4a80dbf9897e35817036c72a1ddc312dc
Akash Seth <akashseth31@gmail.com>
2020-01-04 16:17:51 +0530

    Updates

---
+++ precache-manifest.b7f5a6c2be9cd18e5a9a166fce35e8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edbe80e6a02faf79c1e245e937b4dd2",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "c08c07654b0dc4f7a887",
+    "url": "/react-pokedex/static/css/main.09efc496.chunk.css"
+  },
+  {
+    "revision": "af1f428ded2c18c189e4",
+    "url": "/react-pokedex/static/js/2.e7a4b128.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/react-pokedex/static/js/2.e7a4b128.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c08c07654b0dc4f7a887",
+    "url": "/react-pokedex/static/js/main.f8aeb44a.chunk.js"
+  },
+  {
+    "revision": "319f61193ad1cf2cf903",
+    "url": "/react-pokedex/static/js/runtime-main.4c6656cd.js"
+  }
+]);
