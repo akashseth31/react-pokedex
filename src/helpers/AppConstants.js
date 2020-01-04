57b925d9ae11677cc27273fb91fe251d6b84ac09
Akash Seth <akashseth31@gmail.com>
2020-01-04 16:23:40 +0530

    Changed protocol from hhtp to https to resolve CORS issues

--- src/helpers/AppConstants.js
+++ src/helpers/AppConstants.js
@@ -1,6 +1,6 @@
 import error404 from './assets/not-found.png';
 
-export const GET_POKEDEX_DATA = 'http://pokeapi.co/api/v2/pokemon/';
+export const GET_POKEDEX_DATA = 'https://pokeapi.co/api/v2/pokemon/';
 export const LIMIT = 100;
 export const NOT_FOUND = error404;
 
